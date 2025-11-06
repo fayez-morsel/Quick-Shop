36dd87bd55901a33a8601db8455dac84e8a0d536
Fayez Morsel <fayezmorsel77@gmail.com>
2025-11-06 16:49:18 +0200

    types of actions in usestore for filters, cart and ui

--- frontend/src/store/useStore.ts
+++ frontend/src/store/useStore.ts
@@ -1,11 +1,27 @@
 import { create } from "zustand";
-import type { Product, CartItem, FilterState,UIState } from "../types";
+import type { Product, CartItem, FilterState, UIState } from "../types";
 
 //types for state and action
 
 type State = {
-    products: Product[];
-    filters: FilterState;
-    cart: CartItem[];
-    ui: UIState;
-}
+  products: Product[];
+  filters: FilterState;
+  cart: CartItem[];
+  ui: UIState;
+};
+
+type Actions = {
+  //filters
+  setQuery: (q: string) => void;
+  setSort: (sort: FilterState["sortBy"]) => void;
+  setDiscounted: (v: boolean) => void;
+
+  // cart
+  addtoCart: (id: string) => void;
+  removeFromCart: (id: string) => void;
+  setQty: (id: string, qty: number) => void;
+  clearCart: () => void;
+
+  //ui
+  toggleCart: () => void;
+};
