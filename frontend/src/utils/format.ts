03a91a5045e2e8545f00b963912c115a9e5c123c
Fayez Morsel <fayezmorsel77@gmail.com>
2025-11-06 18:18:54 +0200

    use format to USD

---
+++ frontend/src/utils/format.ts
@@ -0,0 +1,7 @@
+export const money = ( value:  number): string => {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2,
+    }).format(value);
+};
