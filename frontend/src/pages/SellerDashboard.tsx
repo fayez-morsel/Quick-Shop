320f71db1b27e682a11fad1b62e921b849a1e606
Fayez Morsel <fayezmorsel77@gmail.com>
2025-11-14 16:34:27 +0200

    define the options (Category, OrderStatus, Brand)

--- frontend/src/pages/SellerDashboard.tsx
+++ frontend/src/pages/SellerDashboard.tsx
@@ -4,3 +4,41 @@ import type { FormEvent } from "react";
 import { useStore } from "../store/useStore";
 import { money } from "../utils/format";
 import type { Category, OrderStatus, Brand } from "../types";
+
+const categoryOptions: Category[] = [
+  "Tech",
+  "Sound",
+  "Home",
+  "Sport",
+  "Accessories",
+  "Books",
+  "Gifts",
+];
+
+const statusOptions: OrderStatus[] = [
+  "Pending",
+  "Dispatched",
+  "Delivered",
+  "Delivery Unsuccessful",
+  "Canceled",
+];
+
+const brandOptions: Brand[] = [
+  "Tech Hub",
+  "KeyZone",
+  "SoundWave",
+  "DataHub",
+  "ErgoWorks",
+  "HomeLight",
+];
+
+type ProductFormState = {
+  title: string;
+  price: string;
+  compareAtPrice: string;
+  storeId: string;
+  storeName: Brand;
+  category: Category;
+  stock: string;
+  image: string;
+};
