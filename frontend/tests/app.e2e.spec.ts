8761c5732dde21ee470d84819f9f021802e68b7d
Fayez Morsel <fayezmorsel77@gmail.com>
2025-12-05 18:14:22 +0200

    add new test

--- frontend/tests/app.e2e.spec.ts
+++ frontend/tests/app.e2e.spec.ts
@@ -3,11 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('http://localhost:5173/');
   await page.getByRole('button', { name: 'Open menu' }).click();
-  await page.getByRole('button', { name: 'Log in' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).click();
-  await page.getByRole('textbox', { name: 'Email' }).fill('lala@gmail.com');
-  await page.getByRole('textbox', { name: 'Email' }).press('Tab');
-  await page.getByRole('textbox', { name: 'Password' }).fill('11223344');
-  await page.getByRole('button', { name: 'Sign In as Buyer' }).click();
-  await page.getByRole('button', { name: 'Add to Cart' }).nth(5).click();
+  await page.getByRole('button', { name: 'Products', exact: true }).click();
+  await page.getByRole('checkbox', { name: 'accessories' }).check();
+  await page.getByRole('checkbox', { name: 'accessories' }).uncheck();
 });
