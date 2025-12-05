d2fec59bca08baf05829b25a702d2233ba005c77
Fayez Morsel <fayezmorsel77@gmail.com>
2025-12-05 17:02:40 +0200

    routes for product

---
+++ backend/src/routes/productRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { getAllProducts, getProductById, createProduct } from "../controllers/productController.js";
+import { auth } from "../middleware/auth.js";
+
+const router = Router();
+
+router.get("/", getAllProducts);
+router.get("/:id", getProductById);
+router.post("/", auth, createProduct);
+
+export default router;
