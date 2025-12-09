c63918fd2a1127b1ab7b9404a99d40ee821d067c
Fayez Morsel <fayezmorsel77@gmail.com>
2025-12-09 19:54:42 +0200

    re-exports all zustand store

---
+++ frontend/src/store/index.ts
@@ -0,0 +1,7 @@
+export * from "./useAuthStore";
+export * from "./useProductStore";
+export * from "./useCartStore";
+export * from "./useFavoriteStore";
+export * from "./useOrderStore";
+export * from "./useUIStore";
+export * from "./shared";
